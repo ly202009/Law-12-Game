c2caa78b69dee907631795c7aef7dbac8ec097bc
ly202009 <leeyayoh@gmail.com>
2023-12-01 23:15:17 -0600

    Add empty project

---
+++ Law 12 Terrorism Showcase/Assets/Scripts/ScriptWriter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptWriter : MonoBehaviour
+{
+    public Text subtitles;
+    public List<string> locations;
+    public List<string> names;
+    public List<string> action;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
